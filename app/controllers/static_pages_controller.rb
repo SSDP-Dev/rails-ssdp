e108270e11269ccab938355755a00d8a5d146f8e
Tyler Williams <tyler@ogdenstudios.xyz>
2018-12-02 10:23:16 -0700

    add static page methods

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,65 @@
 class StaticPagesController < ApplicationController
+
+  def index
+  end
+
+  def advisory_council
+  end
+
+  def alumni
+  end
+
+  def alumni_members
+  end
+
+  def bylaws
+  end
+
+  def campaigns
+  end
+
+  def chapter_faq
+  end
+
+  def dare
+  end
+
+  def gradebook
+  end
+
+  def history
+  end
+
+  def join
+  end
+
+  def join_survey
+  end
+
+  def justsayknow
+  end
+
+  def mailing_list
+  end
+
+  def materials
+  end
+
+  def mission
+  end
+
+  def movement
+  end
+
+  def reports
+  end
+
+  def resources
+  end
+
+  def staff
+  end
+
+  def strategy
+  end
 end
